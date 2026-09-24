2c5af645940acc8c8f4c3a19b30cf16e9af6feff
Nyangu-Chimzy <mubanganyangu3@gmail.com>
2026-09-24 18:23:22 +0200

    First JavaFX Lab Complete

---
+++ src/main/java/com/example/hellofx/HelloJavaFX.java
@@ -0,0 +1,35 @@
+package com.example.hellofx;
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.control.Label;
+public class HelloJavaFX extends Application {
+    @Override
+    public void start(Stage stage) {
+        Label message  = new Label("Welcome, Nyangu Mubanga!!!");
+        Button button = new Button("Start");
+        Button newbutton = new Button( "Reset" );
+
+        button.setOnAction(event ->
+                message.setText("Great! You clicked the button.")
+        );
+        newbutton.setOnAction(event ->
+                message.setText("Welcome, Nyangu Mubanga!!!")
+        );
+
+        VBox layout = new VBox(20);
+        layout.setAlignment(Pos.CENTER);
+        layout.getChildren().addAll(message, button, newbutton);
+
+        Scene scene = new Scene(layout,500,300);
+        stage.setTitle("My First JavaFX  Application - 202509470");
+        stage.setScene(scene);
+        stage.show();
+    }
+    public static void main (String[]args){
+        launch(args);
+    }
+}
